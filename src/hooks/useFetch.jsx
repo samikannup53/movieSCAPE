4d036985f8f293e94aef82aba904182e56681cfd
samikannup53 <samikannup53@gmail.com>
2025-04-01 19:06:54 +0530

    Resolved HTTPS Error

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export const useFetch = (apiPath, stream, queryTerm = "") => {
   const [data, setData] = useState([]);
   const key = import.meta.env.VITE_API_KEY;
-  const url = `http://www.omdbapi.com/?apikey=${key}&s=${apiPath}&type=${stream}`;
+  const url = `https://www.omdbapi.com/?apikey=${key}&s=${apiPath}&type=${stream}`;
   useEffect(() => {
     async function fetchMovies() {
       fetch(url)
@@ -13,5 +13,5 @@ export const useFetch = (apiPath, stream, queryTerm = "") => {
     fetchMovies();
   }, [url]);
 
-  return { data };
+  return { data };
 };
