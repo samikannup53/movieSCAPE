3a6398f6c49d619aa1cc8a706580dfe7450e67c9
samikannup53 <samikannup53@gmail.com>
2025-04-03 01:19:26 +0530

    Minor UI Corrections

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -8,7 +8,10 @@ export const Favourites = () => {
   return (
     <section className="z-40 bg-[rgb(0,0,0)] text-[rgb(245,197,24)]">
       <div className="container mx-auto p-6">
-      <h1 className="text-xl my-4 border-b pb-2">Favourites</h1>
+        <h1 className="text-2xl my-4 border-b border-[rgb(245,197,24)] pb-2 text-white flex flex-items-center">
+          <span className="text-[rgb(245,197,24)]">| &nbsp;</span>Favourites
+          <span className="text-3xl">&nbsp; &gt;</span>
+        </h1>
         <div className="">
           {favourites.length === 0 ? (
             <p>No favourite movies yet.</p>
