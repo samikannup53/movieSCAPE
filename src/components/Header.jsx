58067c108f3172cdce5b9e50f28692a9724439a8
samikannup53 <samikannup53@gmail.com>
2025-04-04 22:50:41 +0530

    Minor Alignment Corrections in Mobile Swarch Bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -157,7 +157,7 @@ export const Header = () => {
           <input
             type="search"
             placeholder="Search Movies ..."
-            className="outline-none"
+            className="outline-none w-40"
             value={queryTerm}
             onChange={(e) => setQueryTerm(e.target.value)}
           />
